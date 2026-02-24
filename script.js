597f341747a98a53e3d10c2d5e95d7008d0a2c2d
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-02-24 15:58:04 +0530

    2nd class

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("hello world")
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = false;
+// console.log(num);
+// console.log(typeof(num));
+// num = false ;
+
+// num = null ;
+// console.log(num);
+// console.log(typeof(num));
+// num = false ;
+
+
+// num = undefined ;
+// console.log(num);
+// console.log(typeof(num));
+// num = false ;
+
+// var sayHello = function(){
+//     alert("hello world");
+// }
+
+// var numberArray = [1,2,3];
+
+// var animals = new Array["cat" , "dog "];
+
+// var person = {
+//     name = ""
+// }
+
+const sym1 = Symbol(4);
+console.log(sym1);
+
+const sym2 = Symbol(4);
+console.log(sym2);
+
+if(sym1==sym2){
+    console.log("true");
+}
+
+else{
+    console.log("false");
+}
---
+++ script2.js
@@ -0,0 +1,27 @@
+console.log("hello world")
+document.write("hello world")
+
+// when variable dont store any value is known to be undefined(type hi clear nhi hoga)
+// null is a value but it cant hold anything just to avoid the garbage.
+
+// javaS is case sensitive 
+
+// in modern js there are two ways to define the variable ..........that is(( let lastname and const))
+
+// local scope is better than gobal scope and both let and const are in local scope 
+
+// initilization in const have to be in the same line of assiging the value 
+
+//  and we can't redeclare the value in let 
+
+//  primitive data type are ----> number , string , boolean , undefined , null .
+
+
+// reference (object)type ----> collection of name and value 
+// exapmple --> array , function , obejct .
+
+// for reusability we create the function..    baar baar use karne ke liye.
+
+//   ( = ) to assign the value 
+//   ( == ) to compare the value 
+//   ( === ) strict type checking 
