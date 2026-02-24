675d554beda454417b4cbf571a7b76f8da23f783
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-02-24 16:18:13 +0530

    2nd class part 2

--- script.js
+++ script.js
@@ -30,16 +30,33 @@
 //     name = ""
 // }
 
-const sym1 = Symbol(4);
-console.log(sym1);
+// const sym1 = Symbol(4);
+// console.log(sym1);
 
-const sym2 = Symbol(4);
-console.log(sym2);
+// const sym2 = Symbol(4);
+// console.log(sym2);
 
-if(sym1==sym2){
-    console.log("true");
+// if(sym1==sym2){
+//     console.log("true");
+// }
+
+// else{
+//     console.log("false");
+// }
+
+
+// var i ;
+
+// for ( i = 0 ; i < 3 ; i+= 1){
+//     console.log(i);
+// }
+
+
+var count ;
+console.log("starting loop" + "<br/>");
+
+for(count = 0 ; count < 10 ; count++){
+    console.log("current count :" + count + "<br/>");
 }
+console.log("ending loop " + "<br/>");
 
-else{
-    console.log("false");
-}
