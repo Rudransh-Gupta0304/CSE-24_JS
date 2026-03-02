234029fdcb78311359ec69184989476f8fba8ea8
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-03-02 14:20:32 +0530

    for-loop

---
+++ loop.js
@@ -0,0 +1,8 @@
+var count ;
+console.log("starting loop" + "<br/>");
+
+for(count = 0 ; count < 10 ; count++){
+    console.log("current count :" + count + "<br/>");
+}
+console.log("ending loop " + "<br/>");
+
