b2668f9ea8b862afdafc081a7c74f32dc914d5df
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-03-17 20:38:28 +0530

    js.html

--- D_o_m.js
+++ D_o_m.js
@@ -1,46 +1,88 @@
 
-// let x = document.getElementById("demo");
-// x.innerHTML = "hello world";
+let x = document.getElementById("demo");
+x.innerHTML = "hello world";
 
-// let y = document.getElementsByClassName("para");
+let y = document.getElementsByClassName("para");
 
-// let z = document.getElementsByTagName(h3);
+let z = document.getElementsByTagName(h3);
 
-// document.write(y);
-// document.write(z);
+document.write(y);
+document.write(z);
 
 
 // it return the first value
 
-// let a = document.querySelector("#demo");
-// document.write(a);
+let a = document.querySelector("#demo");
+document.write(a);
 
 // it return all values
 
-// let b = document.querySelectorAll("#demo");
-// document.write(b);
+let b = document.querySelectorAll("#demo");
+document.write(b);
 
 
 // get and set attribute
-// let c = document.getElementById("demo").style.color= "red";
-// document.write(c);
+let c = document.getElementById("demo").style.color= "red";
+document.write(c);
 
 
-// let d = document.getElementById("demo").setAttribute("class" , "democlass");
-// console.log(d);
+let d = document.getElementById("demo").setAttribute("class" , "democlass");
+console.log(d);
 
 
 // create element.
 
-// let e = document.createElement("h2");
-// e.innerHTML = "this is the updated one";
-// let parent = document.getElementsByTagName("body");
-// par[0].appeend(e);
+let e = document.createElement("h2");
+e.innerHTML = "this is the updated one";
+let parent = document.getElementsByTagName("body");
+par[0].appeend(e);
 
 
 //  to remove something...
-// e.remove();
+e.remove();
 
 function onClick(){
     alert("happend");
+}
+function onMouseover(){
+    alert("happened");
+}
+function onMouseout(){
+    alert("happened");
+}
+function onMousedown(){
+    alert("happened");
+}
+function onMouseup(){
+    alert("happened");
+}
+function onMousemove(){
+    alert("moving");
+}
+function onDblclick(){
+    alert("happened");
+}
+function onKeydown(){
+    alert("onkeydown");
+}
+function onKeyup(){
+    alert("on key up");
+}
+function onSubmit(){
+    alert("onsubmit");
+}
+function onfocus(){
+    alert("onfocus");
+}
+function onblur(){
+    alert("onblur");
+}
+function onchange(){
+    alert("onchange");
+}
+function onload(){
+    alert("onload");
+}
+function onunload(){
+    alert("unload");
 }
