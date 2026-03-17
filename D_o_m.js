a001f6f99cf73b95cb68b582959c49f4bf566fa3
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-03-17 16:28:33 +0530

    DOM

---
+++ D_o_m.js
@@ -0,0 +1,46 @@
+
+// let x = document.getElementById("demo");
+// x.innerHTML = "hello world";
+
+// let y = document.getElementsByClassName("para");
+
+// let z = document.getElementsByTagName(h3);
+
+// document.write(y);
+// document.write(z);
+
+
+// it return the first value
+
+// let a = document.querySelector("#demo");
+// document.write(a);
+
+// it return all values
+
+// let b = document.querySelectorAll("#demo");
+// document.write(b);
+
+
+// get and set attribute
+// let c = document.getElementById("demo").style.color= "red";
+// document.write(c);
+
+
+// let d = document.getElementById("demo").setAttribute("class" , "democlass");
+// console.log(d);
+
+
+// create element.
+
+// let e = document.createElement("h2");
+// e.innerHTML = "this is the updated one";
+// let parent = document.getElementsByTagName("body");
+// par[0].appeend(e);
+
+
+//  to remove something...
+// e.remove();
+
+function onClick(){
+    alert("happend");
+}
