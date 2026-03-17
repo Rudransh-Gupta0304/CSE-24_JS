3f9580704ebd4baf7b2af72ae00d12d064f5fa97
Rudransh-Gupta0304 <rudransh.25b01010390@abes.ac.in>
2026-03-17 15:31:29 +0530

    Function

---
+++ array.js
@@ -0,0 +1,16 @@
+var myinfo = new Array();
+var myinfo1 = Array();
+
+
+let collection = {
+
+}
+
+// var trainerinfo = ["jitendra" , 29,"ghaziabad" , (isMarried:true)];
+
+
+
+
+
+
+
--- function.js
+++ function.js
@@ -38,4 +38,25 @@
 // console.log(8+9);
 
 
+//  object 
 
+// var person = {
+//     name : "John mac" ,
+//     age : 30 ,
+//     isMarried : true,
+//     child : ['cheeku '];
+//     address : {
+//         street : "vijay nagar",
+//         house : 5 ,
+//     }
+// }
+
+var person2 = new Object();
+person2.name = 'krishna';
+person2.age = 35;
+person2.isMarried = true ;
+person2.address = {};
+person2.address.street = "vijay nagar";
+// format change krke bhi likh skte hai square bracket me krke 
+person2['address']['flatNO'] = 33;
+console.log()
